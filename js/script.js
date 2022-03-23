2feb161c82c18336f26b6144bc4f8ff99e98cf64
DrummyG <balestrinodavide@gmail.com>
2022-03-23 15:22:40 +0100

    secconda aggiunta di bonus

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ const casual = 21;
 document.getElementById('summa') .innerHTML  = userName + surName + color + casual;
 
 // [![](https://mermaid.ink/img/pako:eNqNk0tOwzAQhq9ieU0vkAUIaKFvHmXndGE509QisaOxgwRtb8QtuBiTSR8RolJ3v2a-mX_k_NlI4zOQicxRV-vUCXGrrLNf1gsIgKZRS9HrXYs7VUCMNWphvAuQO71scKr_z9-rELFuJ0pwURfQQXj0nsG-0hV68WFDrYsQ6-zQ7nN7oAyCF8dlbW_AvQcVDNoPL2whKrQlYZA3ZvsND0w9bla1I1Onb3ZcfuTyNvx8b8Wws-IwLEJtDIRA5WV3wPmtGCmjnYGi4Al6PGughUbtSayHrMd_jccd48klxuOT8fSs8ZShIesJ69nRuPWddXznl_jOTr5PZ32fGJqwnrN-VsaXFeXEi0LTJ4Nw_GDPDLwQ4CL6ZlsODpBS0fZfuP-6MVYE7yhNiB7t_t1eO_cvaAUi5Pn-1EW7uMs1Z7-plXWwlFeyBCy1zSjjmwZKZVxDCalMSGYa31OZuh1xdZXpCIPMRo8yWVEW4UrqOvrFpzMyofzBAepbTf9Luad2v2szFcE)](https://mermaid.live/edit#pako:eNqNk0tOwzAQhq9ieU0vkAUIaKFvHmXndGE509QisaOxgwRtb8QtuBiTSR8RolJ3v2a-mX_k_NlI4zOQicxRV-vUCXGrrLNf1gsIgKZRS9HrXYs7VUCMNWphvAuQO71scKr_z9-rELFuJ0pwURfQQXj0nsG-0hV68WFDrYsQ6-zQ7nN7oAyCF8dlbW_AvQcVDNoPL2whKrQlYZA3ZvsND0w9bla1I1Onb3ZcfuTyNvx8b8Wws-IwLEJtDIRA5WV3wPmtGCmjnYGi4Al6PGughUbtSayHrMd_jccd48klxuOT8fSs8ZShIesJ69nRuPWddXznl_jOTr5PZ32fGJqwnrN-VsaXFeXEi0LTJ4Nw_GDPDLwQ4CL6ZlsODpBS0fZfuP-6MVYE7yhNiB7t_t1eO_cvaAUi5Pn-1EW7uMs1Z7-plXWwlFeyBCy1zSjjmwZKZVxDCalMSGYa31OZuh1xdZXpCIPMRo8yWVEW4UrqOvrFpzMyofzBAepbTf9Luad2v2szFcE)
+
+//    A[inizio esercizio] --> B[lettura consegna]
+//    B[inizio esercizio] --> C[struttura mentale esercizio]
+//    C --> D[apro visualstudio]
+//    D --> E[creo struttura]
+//    E --> F[scrivo il primo segmento]
+//    F --> G{funziona?}
+//    G --> |sì| H[scrivo il segmento successivo]
+//    G --> |no| I[cancello il codice]
+//    I --> F
+//    H --> J{funziona?}
+//    J --> |sì| K[scrivo il segmento successivo]
+//    J --> |no| L[cancello il codice]
+//    L --> H
+//    K --> M{funziona}
+//    M --> |sì| N[scrivo il segmento successivo]
+//    M --> |no| O[cancello il codice]
+//    O --> K
+//    N --> P[completo la stesura]
+//    P --> Q[controllo generale]
+//    Q --> R{ci sono errori?}
+//    R --> |sì| S[correggo]
+//    S --> Q
+//    R --> |no| T[fine]
