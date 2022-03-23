af98a6f5ff088571c282e78dcb6aecc1f9c31516
DrummyG <balestrinodavide@gmail.com>
2022-03-23 13:47:54 +0100

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let userName = prompt('Come ti chiami?');
+let surName = prompt('Qual\'è il tuo cognome?');
+let color = prompt('Dimmi il tuo colore preferito');
+const casual = 21;
+
+document.getElementById('summa') .innerHTML  = userName + surName + color + casual;
